1db62578c44bb2eb643c7a624b0396fb54bfd4de
Pascal Gremaud <polchky@gmail.com>
2020-03-27 19:03:52 +0100

    Adapt client resource to existing model

--- models/client.js
+++ models/client.js
@@ -4,6 +4,8 @@ const autoIncrement = require('mongoose-auto-increment');
 const clientSchema = new Mongoose.Schema(
     {
         name: { type: String, required: true },
+        isAdmin: { type: Boolean, required: true },
+        isActive: { type: Boolean, required: true },
     },
     { strict: 'throw' },
 );
