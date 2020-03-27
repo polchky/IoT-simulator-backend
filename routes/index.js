8f6af9a1f7a2651c74bffa1d4d49ebd8a25c7715
Pascal Gremaud <polchky@gmail.com>
2020-03-27 19:30:57 +0100

    Add swagger doc endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const CombineRouters = require('koa-combine-routers');
 const clients = require('@routes/clients');
+const swagger = require('@routes/swagger');
 
 const router = CombineRouters([
     clients,
+    swagger,
 ]);
 
 module.exports = router;
---
+++ routes/swagger.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+const koaSwagger = require('koa2-swagger-ui');
+const YAML = require('yamljs');
+const fs = require('fs');
+
+const router = new Router();
+
+router.use(koaSwagger());
+
+router.get('/doc', koaSwagger({
+    routePrefix: false,
+    swaggerOptions: { spec: YAML.parse(fs.readFileSync('./swagger.yaml', 'utf8')) },
+}));
+
+module.exports = router;
